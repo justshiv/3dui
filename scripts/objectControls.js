d3678abd0d24887526ef4da1f5d4e185cb8d13db
justshiv <accounts@justshiv.com>
2015-08-30 22:04:36 +0200

    incorporating object state for mousemove to easily add rotation movement

--- scripts/objectControls.js
+++ scripts/objectControls.js
@@ -3,38 +3,54 @@
  */
 
 
-
-
-
-
 function onDocumentMouseDown( event ) {
     event.preventDefault();
-    var raycaster = new THREE.Raycaster();
-    raycaster.setFromCamera( mouse, CURRENTCAM );
-    var intersects = raycaster.intersectObjects( objects );
 
-    if ( intersects.length > 0 ) {
+    if ( objstate === OBJSTATE.NONE )
+    {
+        if ( event.button === 0 )
+            objstate = OBJSTATE.ROTATE;
 
-        lockControls();
+        if ( event.button === 2 )
+            objstate = OBJSTATE.MOVE;
+    }
 
-        SELECTED = intersects[ 0 ].object;
-
-        intersects = raycaster.intersectObject( movementPlane );
-        offset.copy( intersects[ 0 ].point ).sub( movementPlane.position );
-        container.style.cursor = 'move';
+    if(objstate === OBJSTATE.ROTATE){
 
     }
+    else if(objstate === OBJSTATE.MOVE){
+        var raycaster = new THREE.Raycaster();
+        raycaster.setFromCamera( mouse, CURRENTCAM );
+        var intersects = raycaster.intersectObjects( objects );
+
+        if ( intersects.length > 0 ) {
+
+            lockControls();
+
+            SELECTED = intersects[ 0 ].object;
+
+            intersects = raycaster.intersectObject( movementPlane );
+            offset.copy( intersects[ 0 ].point ).sub( movementPlane.position );
+            container.style.cursor = 'move';
+
+        }
+    }
+
+    console.log(objstate);
 }
 
 function onDocumentMouseUp( event ) {
     event.preventDefault();
 
-    releaseControls();
+    if(objstate === OBJSTATE.MOVE){
+        releaseControls();
 
-    if ( INTERSECTED ) {
-        movementPlane.position.copy( INTERSECTED.position );
-        SELECTED = null;
+        if ( INTERSECTED ) {
+            movementPlane.position.copy( INTERSECTED.position );
+            SELECTED = null;
+        }
+        container.style.cursor = '-webkit-grab';
     }
-    container.style.cursor = '-webkit-grab';
+    objstate = OBJSTATE.NONE;
 
 }
